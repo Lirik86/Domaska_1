9a4052726b192a943acfec7329166eb39df013e5
lirik <lightlirikus@yandex.ru>
2023-02-11 14:13:09 +0300

    Выполнил 3 задание

---
+++ zadanie_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введи число: ");
+string number11 = Console.ReadLine();
+int number1 = Convert.ToInt32(number11);
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
