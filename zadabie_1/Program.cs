78ae65e8acb1de485d7e2342e6ce3451bcafd881
lirik <lightlirikus@yandex.ru>
2023-02-11 13:38:14 +0300

    Выполнил задание 1

--- zadabie_1/Program.cs
+++ zadabie_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+string number11 = Console.ReadLine();
+int number1 = Convert.ToInt32(number11);
+Console.WriteLine("Введите второе число: ");
+string number22 = Console.ReadLine();
+int number2 = Convert.ToInt32(number22);
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} больше чем {number2}");
+}
+if (number2 > number1)
+{
+    Console.WriteLine($"{number2} больше чем {number1}");
+}
+if (number1 == number2)
+{
+    Console.WriteLine($"{number1} равно {number2}");
+}
